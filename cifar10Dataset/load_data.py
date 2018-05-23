a7f2634d2acdf5798e663dfa0ee44d750d417fd6
snail <945107418@qq.com>
2018-05-23 15:54:15 +0800

    updata files

--- cifar10Dataset/load_data.py
+++ cifar10Dataset/load_data.py
@@ -38,6 +38,24 @@ def read_data(filename,data_path,shape=None,color='RGB'):
         #label = np.zeros(count,dtype=np.uint8)
         lst = [ln.split(' ')[0] for ln in lines]
         label = [int(ln.spilt(' ')[1]) for ln in lines]
+                 
+        idx = 0
+        s,c = SHAPE,CHANNEL_LEN
+        for ln in lines:
+            fname,lab = ln.split(' ')
+            im = imread(os.path.join(data_path,fname),shape=s,color='RGB')
+            '''
+            im = cv2.imread(os.path.join(data_path,fname),cv2.IMREAD_UNCHANGED)
+            im = cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
+            im = cv2.resize(im,(s,s))
+            '''
+            data[idx,:c] = np.reshape(im[:,:,0],c)
+            data[idx,c:2*c] = np.reshape(im[:,:,1],c)
+            data[idx,2*c:] = np.reshape(im[:,:,2],c)
+            label[idx] = int(lab)
+            idx = idx+1
+            
+        return data,label,lst
         
         
         
